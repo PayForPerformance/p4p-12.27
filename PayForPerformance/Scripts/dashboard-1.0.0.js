51023de54b49fb6adeca2956d6df068b5a035440
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-12-27 16:57:06 -0500

    add old files
    still testing rootScope for yearsCtrl

---
+++ PayForPerformance/Scripts/dashboard-1.0.0.js
@@ -0,0 +1,89 @@
+/// <reference path="dashboardApi-1.0.0.js" />
+// JavaScript source code
+
+$(function () {
+
+  $(document).bind('mobileinit', function () {
+    $.mobile.selectmenu.prototype.options.nativeMenu = false;
+  });
+
+  $(".fiscalYearListItem").click(function () {
+    var test = this;
+    $('.fiscalYearLabel').text($(this).text());
+    $('#fiscalYearPopup').popup("close");
+
+  });
+
+  var baseUrl = window.location.protocol + '//' + window.location.hostname + "/";
+  var sourceUrl = "/Scripts/encounterDetail.js";
+  var summaryUrl = "/Scripts/summary.js";
+  var summarySourceUrl = baseUrl + "PayForPerformanceWebApi/api/summary"
+  var encounterDetailSourceUrl = baseUrl + "PayForPerformanceWebApi/api/encounterDetail"
+    try {
+        $.getJSON(summarySourceUrl, function (summaryData) {
+            $('#tcm-01-01').on('click', function () {
+                setTimeout(function () { //Delay until chart container is visible in DOM. 
+                    comharApp.dxChart.tcmChart01($('#chart2-TCM-01-01'), summaryData)
+                }, 1);
+
+                comharApp.highCharts.tcmChart0102($('#chart-TCM-01-02'), summaryData);
+            });
+
+            $('#tcm-02-01').click(function () {
+                comharApp.highCharts.tcmChart0201($('#chart-TCM-02-01'), summaryData);
+            });
+
+            $('#tcm-03-01').on('click', function () {
+                comharApp.highCharts.tcmChart0301($('#chart-TCM-03-01'), summaryData);
+            });
+            $('#tcm-04-01').on('click', function () {
+                comharApp.highCharts.tcmChart0401($('#chart-TCM-04-01'), summaryData);
+            });
+        });
+    }
+    catch (err)
+    {
+        console.log(err);
+    }
+
+
+    $.getJSON(sourceUrl, function (encounterData) {
+      fixedDataSet = comharApp.fixDates(encounterData);
+      comharApp.setGrid($('#gridContainer'));
+    
+      setTimeout(function(){ //Color Data Initially
+        comharApp.colorData();
+      }, 1000);
+    
+      $('.dx-pages').click(function (){ //Set Event Listener for Pagination
+      
+      setTimeout(function() {
+        comharApp.colorData();
+      }, 50);
+    });
+
+    $('#download-CSV').click(function(){
+      var jsonString = JSON.stringify(encounterData);
+      var csvString = csvConverter.convertCSV(jsonString);
+      if (Object.hasOwnProperty.call(window, "ActiveXObject") && !window.ActiveXObject) {  // Determine if client is IE11
+         
+        var blob = new Blob([csvString],{
+        type: "text/csv;charset=utf-8;"
+        });
+        window.navigator.msSaveBlob(blob, "tcm-01.csv");
+
+      } else {
+
+        window.open("data:text/csv;charset=utf-8," + escape(csvString));
+     }
+    });
+    
+    $('.dx-datagrid-action-cursor').on('click', function(){
+       setTimeout(function() {
+        comharApp.colorData();
+      }, 50);
+    })
+  }).fail(function(err) {
+    console.log("error");
+  });
+});
---
+++ PayForPerformance/Scripts/jasmine/spec/ComharAppSpec.js
@@ -0,0 +1,66 @@
+﻿// Reference Scripts/app/controller.js
+
+describe('comharCtrl', function() {
+  var $httpBackend;
+  beforeEach(module('ComharNGApp'));
+
+  var $controller;
+
+
+  beforeEach(inject(function(_$controller_) {
+    $controller = _$controller_;
+  }));
+
+  beforeEach(inject(function(_$httpBackend_) {
+    $httpBackend = _$httpBackend_;
+  }));
+    
+  describe('Loading JSON', function() {
+    it('Loads KPI JSON with a Title', function() {
+      var $scope = {};
+      var controller = $controller('comharCtrl', { $scope : $scope });
+      $httpBackend.expectGET('Scripts/KPIInfo.js').respond({
+        "P4PInfoId":1, 
+        "ProgramName":"TCM", 
+        "KpiId": "TCM-01-01:",
+        "Heading": "Rationale", 
+        "Text" : "To measure the continuity of service provided to CBH-funded TCM and ACT members."
+      });
+      $httpBackend.flush();
+      expect($scope.data.Heading).toEqual("Rationale");
+      
+    });
+   
+    it('Loads Program JSON', function() {
+      var $scope = {};
+      var controller = $controller('programCtrl', { $scope : $scope });
+      $httpBackend.expectGET('Scripts/program.js').respond({
+      "ProgramId":5,"ProgramName":"TCM","ProgramAlias":"ETCM"
+      });
+      $httpBackend.flush();
+      expect($scope.programs.ProgramName).toEqual("TCM");
+    });
+  });
+});
+
+describe('dateCtrl', function(){
+  beforeEach(module('ComharNGApp'));
+
+  var $controller, $scope, $rootScope;
+
+
+  beforeEach(inject(function(_$controller_, _$rootScope_) {
+    $rootScope = _$rootScope_;
+    $controller = _$controller_;
+
+  }));
+
+  describe('Setting The Right Program Date', function() {
+    it('Sets the proper year given the index', function() {
+      var controller = $controller('dateCtrl', {$scope : $rootScope});
+      console.log($rootScope)
+      
+      expect($rootScope.selected).toEqual("2012");
+    });
+  });
+});
