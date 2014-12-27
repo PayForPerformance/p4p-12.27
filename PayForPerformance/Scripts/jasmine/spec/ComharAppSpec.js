9f4d4cdac3459bc0bf5a84fc3ab44c98c668d4f2
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-12-27 18:29:20 -0500

    tests pass for dateCtrl

--- PayForPerformance/Scripts/jasmine/spec/ComharAppSpec.js
+++ PayForPerformance/Scripts/jasmine/spec/ComharAppSpec.js
@@ -46,21 +46,28 @@ describe('comharCtrl', function() {
 describe('dateCtrl', function(){
   beforeEach(module('ComharNGApp'));
 
-  var $controller, $scope, $rootScope;
+  var $controller, scope, $rootScope, $httpBackend;
 
 
-  beforeEach(inject(function(_$controller_, _$rootScope_) {
+  beforeEach(inject(function(_$controller_, _$rootScope_, _$httpBackend_) {
     $rootScope = _$rootScope_;
     $controller = _$controller_;
-
+    scope = $rootScope.$new();
+    $httpBackend = _$httpBackend_
+    $controller = $controller('dateCtrl', {$scope : scope});
+    $httpBackend.expectGET('Scripts/DateLookup.js').respond([{"DataRangeKey":1,"DataRangeName":"2012","FiscalYearStartDate":"2012-01-01T00:00:00","FiscalYearEndDate":"2012-12-31T00:00:00"}, {"DataRangeKey":2,"DataRangeName":"2013","FiscalYearStartDate":"2013-01-01T00:00:00","FiscalYearEndDate":"2013-12-31T00:00:00"}]);
   }));
 
   describe('Setting The Right Program Date', function() {
-    it('Sets the proper year given the index', function() {
-      var controller = $controller('dateCtrl', {$scope : $rootScope});
-      console.log($rootScope)
-      
-      expect($rootScope.selected).toEqual("2012");
+    it('Sets the Initial Year Based on the JSON response', function() {
+      $httpBackend.flush();
+      expect(scope.selected.DataRangeName).toEqual('2012');
+    });
+
+    it('Sets the Correct Year Based on the response given to it', function() {
+      $httpBackend.flush();
+      scope.setSelected(1);
+      expect(scope.selected.DataRangeName).toEqual('2013');
     });
   });
 });
